3fd1e32ee0dfe51083d545d5591f73cd1b834f74
mam37 <mam37@hood.edu>
2015-05-02 03:20:33 +0000

    need to integrate haarcascades and eigenfaces

--- facedetect.py
+++ facedetect.py
@@ -22,12 +22,18 @@ with picamera.PiCamera() as camera:
 				flags = cv2.cv.CV_HAAR_SCALE_IMAGE
 			)
 			for(x,y,w,h) in faces:
-				#draw green rectangle around face detected
+				# draw green rectangle around face detected
 				#cv2.rectangle(img, (x,y), (x+w, y+h), (0, 255,0), 2)
-				#crop image
+				# crop image
 				crop_img = img[y:y+h, x:x+w]
+				# resize image
 				resized = cv2.resize(crop_img, (92, 112), interpolation=cv2.INTER_AREA)
-				cv2.imshow("cropped & resized", resized) 
+				# convert image to grayscale
+				resized = cv2.cvtColor(resized, cv2.COLOR_BGR2GRAY)
+				# equalize histogram
+				resized = cv2.equalizeHist(resized)
+				cv2.imwrite("test_faces/test.pgm", resized) 
+				cv2.imshow("test_faces.pgm", resized) 
 				cv2.waitKey(0)
 
 
